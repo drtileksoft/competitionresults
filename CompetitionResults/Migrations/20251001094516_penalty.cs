dfb0f6f9ab0d11374d60ef9aa4c8656b943b61ce
evildivine <radim.drtilek@gmail.com>
2025-10-01 12:02:17 +0200

    penalty fix

---
+++ CompetitionResults/Migrations/20251001094516_penalty.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CompetitionResults.Migrations
+{
+    /// <inheritdoc />
+    public partial class penalty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "EnableMissingResultPenalty",
+                table: "Competitions",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.UpdateData(
+                table: "Competitions",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "EnableMissingResultPenalty",
+                value: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EnableMissingResultPenalty",
+                table: "Competitions");
+        }
+    }
+}
